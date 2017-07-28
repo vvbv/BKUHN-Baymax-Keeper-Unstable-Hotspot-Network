04c0e5aa1817e3a51b1f088399ee1995d0315c70
vvbv <jeison.cardona@correounivalle.edu.co>
2017-07-28 13:17:44 -0500

    Update keeper-unstable-network-daemon.sh

--- keeper-unstable-network-daemon.sh
+++ keeper-unstable-network-daemon.sh
@@ -2,21 +2,18 @@
 
 while :
 do
-
 	ping -q -c1 8.8.8.8 > /dev/null
 
 	if [ $? -eq 0 ]
 	then
 		echo "Conected"
 		echo "--[New check in 120 seconds]"
-		sleep 120s>/dev/null
 	else
 		echo "Disconected"
 		echo "--[Restarting Network-Manager]"
 		service network-manager restart>/dev/null
 		nmcli radio wifi off>/dev/null
 		nmcli radio wifi on>/dev/null
-		sleep 120s>/dev/null
 	fi
-
+	sleep 120s>/dev/null
 done
